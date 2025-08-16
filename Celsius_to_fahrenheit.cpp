57145d3933dc2d49066547013bc340bffef17221
ROHITH SAJEEV <rohithsajeev19@gmail.com>
2025-08-16 23:35:59 +0530

    Celsius_to_fahrenheit.cpp

--- Celsius_to_fahrenheit.cpp
+++ Celsius_to_fahrenheit.cpp
@@ -6,11 +6,12 @@ double fahrenheit;
 int main()
 {
     cout << "Enter temperature in Celsius: ";
-    cin >> celsius;
+    cin >> celsius;    // inputs the temperature in celsius 
 
-    fahrenheit = (celsius * 1.8) +32;
+    fahrenheit = (celsius * 1.8) +32;    // converts celsius to fahrenheit
 
-    cout << "The temperature in Fahrenheit is " << fahrenheit;
+    cout << "The temperature in Fahrenheit is " << fahrenheit;    // displays the temperate in fahrenheit
     
     return 0;
+
 }

0f063433d4835cd106650f2ab3c7d8ef09348001
ROHITH SAJEEV <rohithsajeev19@gmail.com>
2025-08-16 23:12:04 +0530

    Add files via upload

---
+++ Print_Message.cpp
@@ -0,0 +1,10 @@
+#include <iostream>   //for input and output operations
+
+int main()
+{
+
+    std::cout << "Namah Shivaya!" <<"\n";   // prints message to the console
+
+    return 0;   // indicates successful program termination
+
+}
